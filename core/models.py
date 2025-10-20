a938b10ec0ba3f3951afdf59f58cb76ac5c0114d
Mohammad Tavakoli <mohammadh.tavakoli81@gmail.com>
2025-10-20 20:29:09 +0330

    Create core apps and write utils and pagination and permissions

--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+# Abstract base model that provides self-updating
+# 'created_at' and 'updated_at' fields.
+class TimeStampedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
+
+# Abstract base model that provides soft delete functionality.
+class SoftDeleteModel(models.Model):
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    # Soft delete the instance
+    def soft_delete(self):
+        from django.utils import timezone
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
+
+    # Restore a soft deleted instance.
+    def restore(self):
+        self.is_deleted = False
+        self.deleted_at = None
+        self.save()
